185d428170a9ab8e77a5c4f44cbe0f2e3e8f5797
Hristo <hristo007@gmail.com>
2025-02-24 10:24:08 +0200

    Made server async. Added new "Download Web Content as a File" feature.

--- BasicWebServer/BasicWebServer.Demo/StartUp.cs
+++ BasicWebServer/BasicWebServer.Demo/StartUp.cs
@@ -11,19 +11,27 @@ namespace BasicWebServer.Demo
    Age: <input type='number' name ='Age'/>
 <input type='submit' value ='Save' />
 </form>";
+        private const string DOWNLOAD_FORM = @"<form action='/Content' method='POST'>
+   <input type='submit' value ='Download Sites Content' /> 
+</form>";
+        private const string FILE_NAME = "content.txt";
 
-        public static void Main()
+        public static async Task Main()
         {
+            await DownloadSitesAsTextFileAsync(StartUp.FILE_NAME,
+                new string[] { "https://judge.softuni.org", "https://softuni.org" });
+
             HttpServer server = new HttpServer(routes => routes
-                .MapGet("/", new TextResponse("Hello from the server!"))
-                .MapGet("/HTML", new HtmlResponse(HTML_FORM))
+                .MapGet(" / ", new TextResponse("Hello from the server!"))
                 .MapGet("/Redirect", new RedirectResponse("https://www.softuni.com"))
+                .MapGet("/HTML", new HtmlResponse(HTML_FORM))
                 .MapPost("/HTML", new TextResponse("", StartUp.AddFormDataAction))
+                .MapGet("/Content", new HtmlResponse(StartUp.DOWNLOAD_FORM))
+                .MapPost("/Content", new TextFileResponse(StartUp.FILE_NAME))
             );
 
-            server.Start();
+            await server.StartAsync();
         }
-
         private static void AddFormDataAction(Request request, Response response)
         {
             response.Body = "";
@@ -33,6 +41,33 @@ namespace BasicWebServer.Demo
                 response.Body += $"{key} - {value}";
                 response.Body += Environment.NewLine;
             }
+        }
+        private static async Task<string> DownloadWebSiteContentAsync(string url)
+        {
+            var httpClient = new HttpClient();
+            using (httpClient)
+            {
+                var reponse = await httpClient.GetAsync(url);
+
+                var html = await reponse.Content.ReadAsStringAsync();
+
+                return html.Substring(0, 2000);
+            }
+        }
+        private static async Task DownloadSitesAsTextFileAsync(string fileName, string[] urls)
+        {
+            var downloads = new List<Task<string>>();
+
+            foreach (var url in urls)
+            {
+                downloads.Add(DownloadWebSiteContentAsync(url));
+            }
+
+            var responses  = await Task.WhenAll(downloads);
+
+            var responsesString = string.Join(Environment.NewLine + new String('-', 100), responses);
+
+            await File.WriteAllTextAsync(fileName, responsesString);
         }
     }
 }
